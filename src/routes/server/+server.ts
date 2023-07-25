3624163359ad4d8900b857ff0e3e8f71bab2c957
Jayson Palero <jsonpalero@gmail.com>
2023-07-25 12:45:16 +0800

    Update +server.ts

--- src/routes/server/+server.ts
+++ src/routes/server/+server.ts
@@ -7,9 +7,9 @@ export async function POST({ request }){
     const { firstOperand, secondOperand, operand } = await request.json();
 
     // This return the inputs back to the main pages then convert the strings into numbers then perform the math opration
-    return json(operand == '+' ? Number(firstOperand) + Number(secondOperand) : // This retrun an addition if the operand use or click addbtn on client side
-    operand == '-' ? Number(firstOperand) - Number(secondOperand) : // This retrun an subtaction if the operand use or click  subbtn on client side
-    operand == '*' ? Number(firstOperand) * Number(secondOperand) : // This retrun an multiplication if the operand use or click mulbtn button on client side
-    operand == '/' ? Number(firstOperand) / Number(secondOperand) : // This retrun an addition if the operand use or click add divton on client side
+    return json(operand == '+' ? Number(firstOperand) + Number(secondOperand) : // This return an addition if the operand use or click addbtn on client side
+    operand == '-' ? Number(firstOperand) - Number(secondOperand) : // This return an subtraction if the operand use or click  subbtn on client side
+    operand == '*' ? Number(firstOperand) * Number(secondOperand) : // This return an multiplication if the operand use or click mulbtn button on client side
+    operand == '/' ? Number(firstOperand) / Number(secondOperand) : // This return an divition if the operand use or click add divbtn on client side
     console.log("invalid operator!")); // If not of this math operation ar met. It will return an invalid operator!
 }
